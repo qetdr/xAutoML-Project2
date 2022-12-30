eb92666532f439d482d5e35b984544d30251da2d
qetdr <dmt.roz@gmail.com>
2022-12-30 14:39:58 +0100

    added tpot solution + models for X1 & X2

---
+++ tpot_models/tpot_X2.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+from sklearn.impute import SimpleImputer
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=None)
+
+imputer = SimpleImputer(strategy="median")
+imputer.fit(training_features)
+training_features = imputer.transform(training_features)
+testing_features = imputer.transform(testing_features)
+
+# Average CV score on the training set was: 0.9079855484723574
+exported_pipeline = XGBClassifier(learning_rate=0.001, max_depth=7, min_child_weight=9, n_estimators=100, n_jobs=1, subsample=0.9000000000000001, verbosity=0)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)

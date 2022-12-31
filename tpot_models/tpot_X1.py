5dcb181b791f7762aebd8f1494af579ec7ab21fa
qetdr <dmt.roz@gmail.com>
2022-12-31 01:02:48 +0100

    updated tpot for X1 (60min run)

--- tpot_models/tpot_X1.py
+++ tpot_models/tpot_X1.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 
@@ -15,8 +15,8 @@ imputer.fit(training_features)
 training_features = imputer.transform(training_features)
 testing_features = imputer.transform(testing_features)
 
-# Average CV score on the training set was: 0.9076662750879348
-exported_pipeline = RandomForestClassifier(bootstrap=False, criterion="gini", max_features=0.6500000000000001, min_samples_leaf=16, min_samples_split=8, n_estimators=100)
+# Average CV score on the training set was: 0.9100745054544891
+exported_pipeline = GradientBoostingClassifier(learning_rate=0.1, max_depth=1, max_features=0.4, min_samples_leaf=4, min_samples_split=19, n_estimators=100, subsample=0.35000000000000003)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)

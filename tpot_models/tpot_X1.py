fcaa7f0b2f420d70a8c5587162da3344d0377be6
Allan Mitt <allan.mitt@gmail.com>
2023-01-15 02:11:41 +0200

    undersample update

--- tpot_models/tpot_X1.py
+++ tpot_models/tpot_X1.py
@@ -1,12 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier, GradientBoostingClassifier
-from sklearn.feature_selection import RFE
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
 from tpot.builtins import StackingEstimator
 from xgboost import XGBClassifier
-from sklearn.impute import SimpleImputer
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -15,16 +13,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-imputer = SimpleImputer(strategy="median")
-imputer.fit(training_features)
-training_features = imputer.transform(training_features)
-testing_features = imputer.transform(testing_features)
-
-# Average CV score on the training set was: 0.915828523415523
+# Average CV score on the training set was: 0.7252973218327028
 exported_pipeline = make_pipeline(
-    RFE(estimator=ExtraTreesClassifier(criterion="entropy", max_features=0.7000000000000001, n_estimators=100), step=0.8),
-    StackingEstimator(estimator=GradientBoostingClassifier(learning_rate=0.01, max_depth=1, max_features=0.45, min_samples_leaf=7, min_samples_split=20, n_estimators=100, subsample=0.9500000000000001)),
-    XGBClassifier(learning_rate=0.001, max_depth=3, min_child_weight=9, n_estimators=100, n_jobs=1, subsample=1.0, verbosity=0)
+    StackingEstimator(estimator=ExtraTreesClassifier(bootstrap=False, criterion="gini", max_features=0.9500000000000001, min_samples_leaf=7, min_samples_split=8, n_estimators=100)),
+    XGBClassifier(learning_rate=0.001, max_depth=9, min_child_weight=9, n_estimators=100, n_jobs=1, subsample=0.45, verbosity=0)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
--- tpot_models/tpot_X2.py
+++ tpot_models/tpot_X2.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.svm import LinearSVC
+from sklearn.preprocessing import MinMaxScaler
 from tpot.builtins import StackingEstimator
-from sklearn.impute import SimpleImputer
+from xgboost import XGBClassifier
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -14,15 +14,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-imputer = SimpleImputer(strategy="median")
-imputer.fit(training_features)
-training_features = imputer.transform(training_features)
-testing_features = imputer.transform(testing_features)
-
-# Average CV score on the training set was: 0.9141220482881474
+# Average CV score on the training set was: 0.6656605606489028
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=LinearSVC(C=0.1, dual=False, loss="squared_hinge", penalty="l1", tol=1e-05)),
-    GradientBoostingClassifier(learning_rate=0.1, max_depth=2, max_features=0.15000000000000002, min_samples_leaf=14, min_samples_split=20, n_estimators=100, subsample=0.8)
+    MinMaxScaler(),
+    StackingEstimator(estimator=XGBClassifier(learning_rate=0.01, max_depth=1, min_child_weight=5, n_estimators=100, n_jobs=1, subsample=0.15000000000000002, verbosity=0)),
+    ExtraTreesClassifier(bootstrap=True, criterion="gini", max_features=0.55, min_samples_leaf=4, min_samples_split=9, n_estimators=100)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
